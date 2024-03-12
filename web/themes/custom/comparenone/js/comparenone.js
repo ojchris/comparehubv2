a26e8a41b0b8faee8fce7cae9e51ef436a37271d
Chris Okpada <cokpada37@gmail.com>
2024-03-12 19:37:56 +0000

    Generate custom stable9 sub-theme

---
+++ web/themes/custom/comparenone/js/comparenone.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * comparenone behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.comparenone = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
